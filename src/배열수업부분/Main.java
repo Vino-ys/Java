f094d785d31eda86f43c38482d7f3a23ef2d502c
Vino-ys <yoonsun.yoonsunny@gmail.com>
2026-09-13 23:53:50 +0900

    학교 숙제

---
+++ src/배열수업부분/Main.java
@@ -0,0 +1,13 @@
+package 배열수업부분;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+
+        for(int i = 0; i <= 5; i++) {
+            System.out.print("!");
+            System.out.printf("%d ", a);
+            System.out.println("i = " + i);
+        }
+    }
+}
