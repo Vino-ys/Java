9252d9172164084831a03699a2f26fc5ebdb5b9a
Vino-ys <yoonsun.yoonsunny@gmail.com>
2026-09-22 01:43:28 +0900

    할인 계산 메서드 오류수정 문제 해결

--- src/자바/매서드/문제5.java
+++ src/자바/매서드/문제5.java
@@ -1,7 +1,23 @@
 package 자바.매서드;
 
+class Calculator {
+    public double calculate(int amount) {
+        return amount;
+    }
+}
+
+class DiscountCalculator extends Calculator {
+    public double calculate(double amount) {
+        return amount * 0.9;
+    }
+}
+
 public class 문제5 {
     public static void main(String[] args) {
+        DiscountCalculator calculator = new DiscountCalculator();
 
+        System.out.println(
+                "계산 결과: " + calculator.calculate(10000.0)
+        );
     }
 }
